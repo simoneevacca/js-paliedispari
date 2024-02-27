3da9cd3338038b95370c2dc8d2c7430b66a43e8e
simoneevacca <v.simone.vacca@gmail.com>
2024-02-27 14:17:46 +0100

    add logic palindrome exercise

--- palindromi/assets/js/main.js
+++ palindromi/assets/js/main.js
@@ -1,11 +1,29 @@
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
+
+
 // inserire una parola in una variabile
+
+const word = 'avere';
+console.log(word);
 
 // trasformare la parola in un array
 
+const wordArray = word.split('');
+
 // invertire l'ordine dell'array
 
+const wordArrayReverse = wordArray.reverse()
+
+const wordReverse = wordArrayReverse.join('');
+console.log(wordReverse);
+
 // verificare se l'array invertita è uguale a quella originale
+
+if (word === wordReverse) {
+    console.log('ciao');
+} else {
+    console.log('strunz');
+}
 
 //stampare il risultato
