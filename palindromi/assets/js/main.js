f5bcc9de588c3410288a1df1f08648502fbf365e
simoneevacca <v.simone.vacca@gmail.com>
2024-02-27 13:55:37 +0100

    initial commit

---
+++ palindromi/assets/js/main.js
@@ -0,0 +1,11 @@
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+// inserire una parola in una variabile
+
+// trasformare la parola in un array
+
+// invertire l'ordine dell'array
+
+// verificare se l'array invertita è uguale a quella originale
+
+//stampare il risultato
